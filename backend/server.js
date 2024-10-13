fa5b66e7a7328b0a1b653a28524c4d1664394bf8
Amin <min123kabir@gmail.com>
2024-10-14 00:35:37 +0100

    minor careless mistake

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  //   required: true,
-  //   ca: fs.readFileSync("ca.pem").toString()
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+    required: true,
+    ca: fs.readFileSync("ca.pem").toString()
+  },
 });
 
 //middleware
